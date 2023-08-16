83f7457bdf18623f1af4684efd100810971bbbdd
LeDiepTuyetNhuB1910119 <nhub1910119@gmail.com>
2023-08-16 19:51:26 +0700

    tao frontend co ban

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,61 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 function App() {
   return (
-    <div className="App">
-      <h2>Hello World!</h2>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
